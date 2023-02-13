79987f02e7b027a6b2dd55c5babe6b7a3cbf320c
Rio Edwards <rioredwards@gmail.com>
2023-02-12 17:59:57 -0800

    Subscribe to store in counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
+  // Return the state slice that we need
+  // This automatically subscribes to the store
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
