2375ca399f2bcca038f92b6df6f88b16bff93683
Rio Edwards <rioredwards@gmail.com>
2023-02-12 19:12:16 -0800

    Add increaseAmount to redux store and allow user to manipulate value via a controlled input in the counter compoonent

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import classes from './Counter.module.css';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
@@ -8,13 +9,30 @@ const Counter = () => {
   // Return the state slice that we need
   // This automatically subscribes to the store
   const counter = useSelector((state) => state.counter);
+  const increaseAmount = useSelector((state) => state.increaseAmount);
+  const [inputValue, setInputValue] = useState(increaseAmount);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: increaseAmount });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
+  };
+
+  const increaseAmountHandler = () => {
+    console.log('inputValue', inputValue);
+    dispatch({ type: 'increaseAmount', amount: inputValue });
+  };
+
+  const handleInputChange = (enteredValue) => {
+    if (isNaN(enteredValue)) return;
+    const number = Number(enteredValue);
+    setInputValue(number);
   };
 
   const toggleCounterHandler = () => {};
@@ -22,9 +40,28 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div className={classes.value}>{counter || 0}</div>
+      <div className={classes.value}>{increaseAmount}</div>
+      <form
+        className={classes.inputForm}
+        onSubmit={(e) => {
+          e.preventDefault();
+          increaseAmountHandler();
+        }}
+      >
+        <label htmlFor="amount">Increase Amount</label>
+        <input
+          id="amount"
+          type="text"
+          placeholder="Ex: 5"
+          value={inputValue}
+          onChange={(e) => handleInputChange(e.target.value)}
+        ></input>
+        <button>Submit</button>
+      </form>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,32 @@
 import { legacy_createStore } from 'redux';
 
 // Reducer
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, increaseAmount: 5 }, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      increaseAmount: state.increaseAmount,
+    };
+  }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      increaseAmount: state.increaseAmount,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      increaseAmount: state.increaseAmount,
+    };
+  }
+
+  if (action.type === 'increaseAmount') {
+    return {
+      counter: state.counter,
+      increaseAmount: action.amount,
     };
   }
 
